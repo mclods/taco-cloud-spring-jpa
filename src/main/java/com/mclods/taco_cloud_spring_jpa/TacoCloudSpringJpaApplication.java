f4ba9cd3acf2fb3f5fcd86012042c1e28315b27d
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-04-25 12:29:18 +0530

    First Commit

---
+++ src/main/java/com/mclods/taco_cloud_spring_jpa/TacoCloudSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mclods.taco_cloud_spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mclods/taco_cloud_spring_jpa/TacoCloudSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mclods.taco_cloud_spring_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
